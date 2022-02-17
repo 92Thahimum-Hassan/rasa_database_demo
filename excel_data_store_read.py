31696813b4ab4101ab4aa701f8dd7a93f7ba9e54
92Thahimum-Hassan <thahimumhassanmonami@gmail.com>
2022-02-17 18:54:04 -0500

    Add files via upload

---
+++ excel_data_store_read.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+def DataStore(name, mobile_number, email,occupation):
+    if os.path.isfile("user_data.xslx"):
+        df = pd.read_excel("user_data.xlsx")
+        df.append([name, mobile_number, email,occupation])
+        df.to_excel("user_data.xlsx", index=False)
+
+    else:
+        df = pd.DataFrame([name, mobile_number, email,occupation],
+                          columns=["name", "mobile_number", "email","occupation"])
+        df.to_excel("user_data.xlsx", index=False)
